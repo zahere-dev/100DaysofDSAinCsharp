f433115d86db4bd2f29aa67a2817780309367202
Zahiruddin Tavargere <Zahiruddin_T@Dell.com>
2022-01-09 18:08:32 +0530

    Day2 - part 1 > Construct CircularLinkedList and DoublyLinkedList

--- 100DaysofDSAinCsharp/Code/Day2/Day2Ops.cs
+++ 100DaysofDSAinCsharp/Code/Day2/Day2Ops.cs
@@ -4,49 +4,100 @@
     {
         public static void Run()
         {
+            #region CircularLinkedList
             CircularLinkedList cll = new CircularLinkedList();
 
-            cll.addFirst(3);
-            cll.display();
+            //cll.addFirst(3);
+            //cll.display();
 
-            cll.addFirst(7);
-            cll.display();
+            //cll.addFirst(7);
+            //cll.display();
 
-            cll.addLast(6);
+            //cll.addLast(6);
 
-            cll.display();
-
-
-            cll.addAnywhere(2, 2);
-            cll.display();
-
-            cll.addAnywhere(9, 0);
-            cll.display();
-
-            cll.addFirst(7);
-            cll.addFirst(7);
+            //cll.display();
 
 
-            cll.removeLast();
-            cll.display();
+            //cll.addAnywhere(2, 2);
+            //cll.display();
 
-            cll.removeFirst();
-            cll.display();
+            //cll.addAnywhere(9, 0);
+            //cll.display();
 
-            cll.removeAtGivenPosition(1);
-            cll.display();
+            //cll.addFirst(7);
+            //cll.addFirst(7);
 
-            cll.addFirst(7);
-            cll.addFirst(7);
-            cll.removeAtGivenPosition(5);
-            cll.display();
 
-            cll.removeDuplicates();
-            cll.display();
+            //cll.removeLast();
+            //cll.display();
 
-            cll.reverse_approach1();
-            cll.display();
+            //cll.removeFirst();
+            //cll.display();
+
+            //cll.removeAtGivenPosition(1);
+            //cll.display();
+
+            //cll.addFirst(7);
+            //cll.addFirst(7);
+            //cll.removeAtGivenPosition(5);
+            //cll.display();
+
+            //cll.removeDuplicates();
+            //cll.display();
+
+            //cll.reverse_approach1();
+            //cll.display();
+            #endregion
+
+            #region DoublyLinkedList
+            DoublyLinkedList dll = new DoublyLinkedList();
+            dll.addLast(75);
+            dll.display();
+
+            dll.addFirst(3);
+            dll.addFirst(38);
+            dll.addFirst(85);
+            dll.addFirst(38);
+            dll.addFirst(12);
+            dll.addLast(75);
+            dll.addFirst(45);
+            dll.display();
+
+            dll.addLast(75);
+            dll.addLast(79);
+            dll.display();
+
+            dll.addAnywhere(99, 1);
+            dll.addAnywhere(98, 0);
+            dll.addAnywhere(97, 13);
+            dll.display();
+
+            dll.removeFirst();
+            dll.display();
+
+            dll.removeLast();
+            dll.display();
+
+
+            dll.removeAtGivenPosition(1);
+            dll.display();
+
+            dll.removeAtGivenPosition(0);
+            dll.display();
+
+            dll.removeAtGivenPosition(6);
+            dll.display();
+
+            dll.removeDuplicates();
+            dll.display();
+
+            dll.reverse_approach1();
+            dll.display();
+            #endregion
+
+
         }
 
     }
 }
+
---
+++ 100DaysofDSAinCsharp/Code/Day2/DoublyLinkedList.cs
@@ -0,0 +1,222 @@
+﻿using System.Collections.Generic;
+
+namespace _100DaysofDSAinCsharp.Code.Day2
+{
+    public class NodeDL
+    {
+        public int element;
+        public NodeDL next;
+        public NodeDL previous;
+        public NodeDL(int e)
+        {
+            element = e;
+            next = null;
+            previous = null;
+
+        }
+    }
+
+
+    public class DoublyLinkedList
+    {
+        private NodeDL head;
+        private NodeDL tail;
+        private int size;
+
+        public DoublyLinkedList()
+        {
+            head = null;
+            tail = null;
+            size = 0;
+        }
+
+        public int length()
+        {
+            return size;
+        }
+
+        public bool isEmpty()
+        {
+            return size == 0;
+        }
+
+        public void addFirst(int e)
+        {
+            NodeDL newNode = new NodeDL(e);
+            if (isEmpty())
+            {
+                head = newNode;
+                tail = newNode;
+                tail.previous = newNode;
+            }
+            else
+            {
+                newNode.next = head;
+                head.previous = newNode;
+                head = newNode;
+            }
+            size++;
+        }
+
+        public void addLast(int e)
+        {
+            NodeDL newNode = new NodeDL(e);
+            if (isEmpty())
+            {
+                addFirst(e);
+            }
+            else
+            {
+                newNode.previous = tail;
+                tail.next = newNode;
+                tail = newNode;
+                size++;
+            }
+
+        }
+
+
+        public void addAnywhere(int e, int position)
+        {
+            if (position == 0)
+            {
+                addFirst(e);
+            }
+            else if (position > length() || position < 0)
+            {
+                return;
+            }
+            else
+            {
+                NodeDL newNode = new NodeDL(e);
+
+                var currentNode = head;
+                int counter = 0;
+                while (counter != position - 1)
+                {
+                    currentNode = currentNode.next;
+                    counter++;
+                }
+                newNode.next = currentNode.next;
+                newNode.previous = currentNode;
+                currentNode.next = newNode;
+                currentNode = newNode;
+                size++;
+            }
+
+
+
+        }
+
+        public void removeFirst()
+        {
+            if (isEmpty()) return;
+            head = head.next;
+            head.previous = null;
+            size--;
+        }
+
+        public void removeLast()
+        {
+            if (isEmpty()) return;
+            tail = tail.previous;
+            tail.next = null;
+            size--;
+        }
+
+        //time complexity > worst case O(n); space complexity = O(1)
+        public void removeAtGivenPosition(int position)
+        {
+            if (isEmpty() || position < 0 || position > size)
+            {
+                return;
+            }
+            else if (position == 0)
+            {
+                removeFirst();
+            }
+            else if (position == length())
+            {
+                removeLast();
+            }
+            else
+            {
+                var currentNode = head;
+                int counter = 0;
+                while (counter != position - 1)
+                {
+                    currentNode = currentNode.next;
+                    counter++;
+                }
+                var nextNode = currentNode.next;
+                currentNode.next = nextNode.next;
+                nextNode.previous = currentNode;
+                size--;
+            }
+        }
+
+
+        //time complexity = O(n); space complexity = O(1)
+        public void removeDuplicates()
+        {
+            if (isEmpty()) return;
+
+            var hasMap = new Dictionary<int, int>();
+            var currentNode = head;
+            int counter = 0;
+            while (currentNode != null)
+            {
+                if (hasMap.ContainsKey(currentNode.element))
+                {
+                    removeAtGivenPosition(counter);
+                    counter--;
+                }
+                else
+                {
+                    hasMap.Add(currentNode.element, counter);
+                }
+                currentNode = currentNode.next;
+                counter++;
+            }
+
+        }
+
+        //time complexity = O(n); space complexity = O(1)
+        public void reverse_approach1()
+        {
+            if (isEmpty()) return;
+
+            var currentNode = head;
+            NodeDL prevNode = null;
+            int counter = 0;
+            while (currentNode != null)
+            {
+                var nextNode = currentNode.next;
+                currentNode.next = prevNode;
+                prevNode = currentNode;
+                currentNode = nextNode;
+                counter++;
+            }
+            head = prevNode;
+
+        }
+
+
+        public void display()
+        {
+            if (isEmpty()) return;
+
+            var currentNode = head;
+
+            while (currentNode != null)
+            {
+                System.Console.Write($"{currentNode.element} <-->");
+                currentNode = currentNode.next;
+            }
+            System.Console.WriteLine();
+
+
+        }
+
+    }
+}
