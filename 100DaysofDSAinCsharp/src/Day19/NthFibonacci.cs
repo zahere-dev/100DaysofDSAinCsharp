b46ed075c5a3fb8b3c8c148ba467a8583f07f8ed
Zahiruddin Tavargere <Zahiruddin_T@Dell.com>
2022-02-04 00:30:55 +0530

    Day19

---
+++ 100DaysofDSAinCsharp/src/Day19/NthFibonacci.cs
@@ -0,0 +1,12 @@
+﻿namespace _100DaysofDSAinCsharp.src.Day19
+{
+    public class NthFibonacci
+    {
+        public static int Run(int n)
+        {
+            if (n == 2) return 1;
+            else if (n == 1) return 0;
+            else return Run(n - 1) + Run(n - 2);
+        }
+    }
+}
---
+++ Tests/Day19Tests.cs
@@ -0,0 +1,21 @@
+﻿using _100DaysofDSAinCsharp.src.Day19;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class Day19Tests
+    {
+        [TestMethod]
+        public void BSTTest()
+        {
+
+            //Assert.AreEqual(0, NthFibonacci.Run(0));
+            Assert.AreEqual(1, NthFibonacci.Run(2));
+            Assert.AreEqual(5, NthFibonacci.Run(6));
+
+
+        }
+
+    }
+}
